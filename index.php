d6aa74d6e96b33e3f9acd1faa223b6840ac0513e
Bodryakov.web <bodryakov.web@gmail.com>
2025-08-11 12:19:49 +0300

    .

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ if (count($parts) >= 1 && preg_match('~^(\d+)-([a-z-]+)$~', $parts[0], $m1)) {
                 if (!(int)$ls['is_published']) continue; // скрываем непубликованные
                 $path = '/' . $parts[0] . '/' . $parts[1] . '/' . ((int)$ls['lesson_order']) . '-' . e($ls['slug']);
                 echo '<article class="card card-sm">';
-                echo '<h3><a href="' . $path . '">Урок-' . (int)$ls['lesson_order'] . '-' . e($ls['title_ru']) . '</a></h3>';
+                echo '<h3><a href="' . $path . '">Урок ' . (int)$ls['lesson_order'] . '. ' . e($ls['title_ru']) . '</a></h3>';
                 echo '</article>';
             }
             echo '</div>';
@@ -186,7 +186,7 @@ if (count($parts) >= 1 && preg_match('~^(\d+)-([a-z-]+)$~', $parts[0], $m1)) {
                 ['href' => '/', 'label' => 'Главная'],
                 ['href' => '/' . $parts[0], 'label' => 'Уровень ' . $levelNumber . '. ' . $level['title_ru']],
                 ['href' => '/' . $parts[0] . '/' . $parts[1], 'label' => 'Раздел ' . $sectionOrder . '. ' . $section['title_ru']],
-                ['href' => '', 'label' => 'Урок-' . $lessonOrder . '-' . $lesson['title_ru']],
+                ['href' => '', 'label' => 'Урок ' . $lessonOrder . '. ' . $lesson['title_ru']],
             ]);
             echo '<main class="container lesson">';
             echo '<article class="lesson-body">';
